4fa5cc01e5335f16176cc5bee9e5ba8730289f9f
SAbhinav001 <abhinav.temp01@gmail.com>
2023-08-29 00:52:35 +0530

    Intial Setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const {PORT} = require("./config/server-config")
+
+
+const setupAndStartServer = ()=>{
+    const app = express()
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:true}))
+
+    app.listen(PORT, ()=>{
+        console.log(`server is running on port ${PORT}`)
+    })
+}
+
+setupAndStartServer()
